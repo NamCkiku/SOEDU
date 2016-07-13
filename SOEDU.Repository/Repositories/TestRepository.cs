90460d38f28b5c4c456bf36eca9c4d42acbe1e68
NamCkiku <tranhoangnam11373@gmail.com>
2016-07-13 10:11:39 +0700

    add repository

---
+++ SOEDU.Repository/Repositories/TestRepository.cs
@@ -0,0 +1,22 @@
+﻿using SOEDU.Entities.Models;
+using SOEDU.Repository.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOEDU.Repository.Repositories
+{
+    public interface ITestRepository
+    {
+
+    }
+    class TestRepository : RepositoryBase<Sys_Test>, ITestRepository
+    {
+        public TestRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+}
---
+++ SOEDU.Repository/Repositories/TestSectionRepository.cs
@@ -0,0 +1,16 @@
+﻿using SOEDU.Entities.Models;
+using SOEDU.Repository.Infrastructure;
+
+namespace SOEDU.Repository.Repositories
+{
+    public interface ITestSectionRepository
+    {
+    }
+
+    internal class TestSectionRepository : RepositoryBase<Sys_TestSection>, ITestSectionRepository
+    {
+        public TestSectionRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
