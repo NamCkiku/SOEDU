2a515056eff27fcefee63c4faec61d959513a3af
NamCkiku <tranhoangnam11373@gmail.com>
2016-07-14 14:31:54 +0700

    add auto mapper

---
+++ SOEDU.Api/Models/ViewModels/TargetViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOEDU.Api.Models.ViewModels
+{
+    public class TargetViewModel
+    {
+
+        public string Target_ID { get; set; }
+
+
+        public string Course_ID { get; set; }
+
+
+        public string User_ID { get; set; }
+
+        public string Parent_ID { get; set; }
+
+        public int? IsStar { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public int? IsOrder { get; set; }
+
+        public virtual CourseViewModel Sys_Course { get; set; }
+
+        public virtual ICollection<TargetViewModel> Sys_Target1 { get; set; }
+
+        public virtual TargetViewModel Sys_Target2 { get; set; }
+
+        public virtual UsersViewModel Sys_Users { get; set; }
+    }
+}
---
+++ SOEDU.Api/Models/ViewModels/UsersViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOEDU.Api.Models.ViewModels
+{
+    public class UsersViewModel
+    {
+
+        public string User_ID { get; set; }
+
+
+        public string User_Name { get; set; }
+
+
+        public string IsPassword { get; set; }
+
+        public string IsName { get; set; }
+
+        public bool? IsSex { get; set; }
+
+
+        public string IsAdds { get; set; }
+
+
+        public string IsEmail { get; set; }
+
+
+        public string IsMobile { get; set; }
+
+        public string IsDes { get; set; }
+
+        public byte[] IsFoto { get; set; }
+
+        public bool? IsLocked { get; set; }
+
+
+        public string IsLevel { get; set; }
+
+
+        public DateTime? CreateDate { get; set; }
+
+
+        public virtual ICollection<StudentCourseViewModel> Sys_StudentCourse { get; set; }
+
+
+        public virtual ICollection<StudentTestViewModel> Sys_StudentTest { get; set; }
+
+        public virtual ICollection<StudentTestSectionViewModel> Sys_StudentTestSection { get; set; }
+
+
+        public virtual ICollection<TargetViewModel> Sys_Target { get; set; }
+
+
+        public virtual ICollection<TeacherCourseViewModel> Sys_TeacherCourse { get; set; }
+    }
+}
