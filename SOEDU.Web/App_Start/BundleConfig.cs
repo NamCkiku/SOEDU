d6465d7d817f506f84c9ef51054a9c268d3caba6
NamCkiku <tranhoangnam11373@gmail.com>
2016-07-15 09:32:22 +0700

    add angularjs

--- SOEDU.Web/App_Start/BundleConfig.cs
+++ SOEDU.Web/App_Start/BundleConfig.cs
@@ -9,39 +9,25 @@ namespace SOEDU.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Content/js/jquery-1.10.2.min.js",
-                        "~/Content/Plugins/bootstrap/dist/js/bootstrap.min.js",
+                        "~/Content/Plugins/bootstrap/dist/js/bootstrap.js",
                         "~/Content/js/main.js",
                         "~/Content/js/jquery.scrollUp.min.js",
                         "~/Content/js/owl.carousel.min.js"
                         ));
 
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/Plugins/bootstrap/dist/css/bootstrap.min.css",
-                "~/Content/css/Style.css",
-                "~/Content/Plugins/font-awesome-4.5.0/css/font-awesome.min.css",
-                "~/Content/Plugins/bootstrap-social-gh-pages/bootstrap-social.css",
-                "~/Content/css/owl.carousel.css"
-                ));
+                        "~/Content/Plugins/bootstrap/dist/css/bootstrap.css",
+                        "~/Content/css/Style.css",
+                        "~/Content/Plugins/font-awesome-4.5.0/css/font-awesome.min.css",
+                        "~/Content/Plugins/bootstrap-social-gh-pages/bootstrap-social.css",
+                        "~/Content/css/owl.carousel.css"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Content/Plugins/angular/angular.js"
+                        ));
         }
     }
 }
